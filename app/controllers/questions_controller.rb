71bfe8cc6b5b130dab20866b1d809d49c5daeb3d
Charlie <158181191+Charlie2222223@users.noreply.github.com>
2025-03-14 11:16:21 +0900

    first commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,44 @@
+class QuestionsController < ApplicationController
+  # 質問一覧表示
+  def index
+
+  end
+
+  # 質問詳細ページの表示
+  def show
+    # p.to_param[:id]
+    @question = Question.find(params[:id])
+  end
+
+  # 質問の作成
+  def new
+    @question = Question.new
+  end
+
+  # 質問の登録
+  def create
+    # Questionモデルを初期化
+    @question = Question.new(question_params)
+    # QuestionモデルをDBに保存
+    @question.save
+    # showへリダイレクト
+    redirect_to @question
+  end
+
+  # 質問の編集
+  def edit
+  end
+
+  # 質問の更新
+  def update
+  end
+
+  # 質問の削除
+  def destroy
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :name, :content)   # データベースに保存する処理を指定
+  end
+end
